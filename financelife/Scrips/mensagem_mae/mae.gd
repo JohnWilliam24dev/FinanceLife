eff0d446aa3d964807a219470c3361d4d0bbfafd
Insaniis <hall3dead@gmail.com>
2024-12-06 23:39:55 -0300

    Botão e Cartas
    Tem um bug quando girar o turno do jogo, a carta se desloca para a direita, mas se a tela estiver minimizada a carta não fica correndo.

--- financelife/Scrips/mensagem_mae/mae.gd
+++ financelife/Scrips/mensagem_mae/mae.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var label: Label = $Label
 
-
 var player_in_area: bool = false
 var awaiting_choice: bool = false
 
@@ -11,23 +10,21 @@ func _process(delta: float) -> void:
 		label.text = "Você quer jogar? (Sim(S))/(Não(N))"
 		awaiting_choice = true
 
-	#if awaiting_choice:
-		#if Input.is_action_just_pressed("accept"): # Assumindo que "ui_accept" está mapeado para "S"
-			#go_to_battle()
-			#awaiting_choice = false
-		#elif Input.is_action_just_pressed("cancel"): # Assumindo que "ui_cancel" está mapeado para "N"
-			#exit_interaction()
-			#awaiting_choice = false
+	if awaiting_choice:
+		if Input.is_action_just_pressed("accept"): # Assumindo que "ui_accept" está mapeado para "S"
+			go_to_battle()
+			awaiting_choice = false
+		elif Input.is_action_just_pressed("cancel"): # Assumindo que "ui_cancel" está mapeado para "N"
+			exit_interaction()
+			awaiting_choice = false
 
-#func go_to_battle() -> void:
-	#label.text = "Indo para a batalha..."
-	#await(get_tree().create_timer(1.0), "timeout") # Pequeno atraso antes de trocar a cena
-	#get_tree().change_scene("res://CENA_3_BATALHA.tscn")
+func go_to_battle() -> void:
+	label.text = "Indo para a batalha..."
+	get_tree().change_scene_to_file("res://Cenas/JogoDeTabuleiro/jogo_de_tabuleiro.tscn")
 
-#func exit_interaction() -> void:
-	#label.text = "Interação encerrada."
-	#await(get_tree().create_timer(1.5), "timeout")
-	#label.visible = false
+func exit_interaction() -> void:
+	label.text = "Interação encerrada."
+	label.visible = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "CharacterBody2D":
--- financelife/tabuleiro.gd
+++ financelife/tabuleiro.gd
@@ -1,18 +1,26 @@
 extends Control
 class_name tabuleiroclass
 
+@onready var botao_proximo_turno: Button = $BotaoProximoTurno
+
+var _current_carta_sprite: Sprite2D = null
 var acoes_lista: Array = []  
 var saldo: saldos
 var menu: MenuDeAcoes
+
 func D6() -> int:
 	return floor(randf_range(1, 6))
+
 func D66() -> int:
 	return floor(randf_range(-6, 6))
+
 func D8() -> int:
 	return floor(randf_range(4, 8))
+
 func adicionar_acao(nicho: String, preco: float):
 	var nova_acao = acoes.new(nicho, preco)
 	acoes_lista.append(nova_acao)
+
 func inicializar_acoes():
 	adicionar_acao("Transporte", D8())
 	adicionar_acao("Siderúrgica", D8())
@@ -20,51 +28,39 @@ func inicializar_acoes():
 	adicionar_acao("Saúde", D8())
 	adicionar_acao("Alimentação", D8())
 	
-	var label_valor_acao_trans: Label 
-	label_valor_acao_trans = $"SituaçAçãoTrans/ValorAçãoTrans"
-	label_valor_acao_trans.text = "R$ %.2f" % acoes_lista[0].preco
-	
-	var label_valor_acao_side: Label 
-	label_valor_acao_side = $"SituaçAçãoSider/ValorAçãoSider"
-	label_valor_acao_side.text = "R$ %.2f" % acoes_lista[1].preco
-	
-	var label_valor_acao_tecno: Label 
-	label_valor_acao_tecno = $"SituaçAçãoTecno/ValorAçãoTecno"
-	label_valor_acao_tecno.text = "R$ %.2f" % acoes_lista[2].preco
-	
-	var label_valor_acao_saude: Label 
-	label_valor_acao_saude = $"SituaçAçãoSaúde/ValorAçãoSau"
-	label_valor_acao_saude.text = "R$ %.2f" % acoes_lista[3].preco
-	
-	var label_valor_acao_alim: Label 
-	label_valor_acao_alim = $"SituaçAçãoAlimen/ValorAçãoAlime"
-	label_valor_acao_alim.text = "R$ %.2f" % acoes_lista[4].preco
-	
+	atualizar_valores_acoes()
 
+func atualizar_valores_acoes():
+	$"SituaçAçãoTrans/ValorAçãoTrans".text = "R$ %.2f" % acoes_lista[0].preco
+	$"SituaçAçãoSider/ValorAçãoSider".text = "R$ %.2f" % acoes_lista[1].preco
+	$"SituaçAçãoTecno/ValorAçãoTecno".text = "R$ %.2f" % acoes_lista[2].preco
+	$"SituaçAçãoSaúde/ValorAçãoSau".text = "R$ %.2f" % acoes_lista[3].preco
+	$"SituaçAçãoAlimen/ValorAçãoAlime".text = "R$ %.2f" % acoes_lista[4].preco
 
 func subida(acao: acoes) -> void:
 	acao.preco += D6()
 
 func queda(acao: acoes) -> void:
 	acao.preco -= D6()
+
 func volatil(acao: acoes) -> void:
 	acao.preco += D66()
+
 func mudar_valor_acoes(nicho1: String, resultado1: bool):
 	var acao1 = _buscar_acao_por_nicho(nicho1)
-	
-	
 	if not acao1:
-		return  
+		return
 	
-	if resultado1==true:
+	if resultado1 == true:
 		subida(acao1)
 	else:
 		queda(acao1)
+	
 	for acao in acoes_lista:
-		if acao.nicho!=acao1.nicho:
+		if acao.nicho != acao1.nicho:
 			volatil(acao)
-			
-			
+	
+	atualizar_valores_acoes()
 
 func _buscar_acao_por_nicho(nicho: String) -> acoes:
 	for acao in acoes_lista:
@@ -72,41 +68,84 @@ func _buscar_acao_por_nicho(nicho: String) -> acoes:
 			return acao
 	return null
 
+func nova_rodada():
+	if _current_carta_sprite:
+		remove_child(_current_carta_sprite)
+		_current_carta_sprite.queue_free()
+		_current_carta_sprite = null
 
+	var cartas_sorteadas = sortear_cartas(1)
+	var nova_carta = cartas_sorteadas[0]
+
+	mudar_valor_acoes(nova_carta.nicho, nova_carta.informacao)
+	exibir_carta(nova_carta)
+
+func sortear_cartas(quantidade: int) -> Array:
+	var cartas = []
+	var nichos_possiveis = ["Transporte", "Siderúrgica", "Tecnologia", "Saúde", "Alimentação"]
+	for i in range(quantidade):
+		var nicho_aleatorio = nichos_possiveis[randi() % nichos_possiveis.size()]
+		var informacao_aleatoria = randf() > 0.5
+		cartas.append(carta_informacao.new(nicho_aleatorio, informacao_aleatoria))
+	return cartas
+
+func exibir_carta(carta: carta_informacao):
+	var sprite = Sprite2D.new()
+	match carta.nicho:
+		"Transporte":
+			if carta.informacao:
+				sprite.texture = preload("res://assents/Cartas/_Carta em alta Transporte.png")
+			else:
+				sprite.texture = preload("res://assents/Cartas/Carta em baixa Transporte.png")
+		"Siderúrgica":
+			if carta.informacao:
+				sprite.texture = preload("res://assents/Cartas/Carta em alta Siderúrgica.png")
+			else:
+				sprite.texture = preload("res://assents/Cartas/Carta em Baixa Siderúrgica.png")
+		"Tecnologia":
+			if carta.informacao:
+				sprite.texture = preload("res://assents/Cartas/Carta em alta Tecnologia.png")
+			else:
+				sprite.texture = preload("res://assents/Cartas/Cópia de Carta em Baixa Tecnologia.png")
+		"Saúde":
+			if carta.informacao:
+				sprite.texture = preload("res://assents/Cartas/_ Carta em alta Saúde.png")
+			else:
+				sprite.texture = preload("res://assents/Cartas/_Carta em baixa Saúde.png")
+		"Alimentação":
+			if carta.informacao:
+				sprite.texture = preload("res://assents/Cartas/Carta em alta Alimentação.png")
+			else:
+				sprite.texture = preload("res://assents/Cartas/Carta em baixa Alimentação.png")
+
+	var janela_tamanho = get_viewport().get_visible_rect().size
+	sprite.position = janela_tamanho - Vector2(sprite.texture.get_size().x * -0.3 + 20, sprite.texture.get_size().y * 0.4 + 20)
+	sprite.scale = Vector2(0.8, 0.8)
+	add_child(sprite)
+	_current_carta_sprite = sprite
 
 func _ready() -> void:
-	var label_saldo_valor: Label #Criar a conexão entre o Label e o codigo
-	label_saldo_valor = $Sprite2D/SaldoValor
-	 
-	saldo = saldos.new(1000.0)  # O jogador começa com 1000 de saldo
-	
+	botao_proximo_turno.connect("pressed", Callable(self, "nova_rodada"))
+	var label_saldo_valor: Label = $Sprite2D/SaldoValor
+	saldo = saldos.new(1000.0)
 	label_saldo_valor.text = "R$ %.2f" % saldo.puxar_saldo()
-	
 
-	# Inicializar menu de ações
 	menu = MenuDeAcoes.new()
 	menu.configurar(self, saldo)
-
-	# Inicializar ações
 	inicializar_acoes()
 
-	# Exibir ações iniciais
 	print("Ações iniciais:")
 	for acao in acoes_lista:
 		print("Nicho: %s, Preço: %f" % [acao.nicho, acao.preco])
 
-	# Cartas fictícias para alterar valores
-	var carta1 = carta_informacao.new("Transporte", true)
-	var carta2 = carta_informacao.new("Saúde", false)
-	mudar_valor_acoes(carta1.nicho, carta1.informacao)
+	var cartas_sorteadas = sortear_cartas(1)
+	for carta in cartas_sorteadas:
+		print("Carta sorteada - Nicho: %s, Impacto Positivo: %s" % [carta.nicho, str(carta.informacao)])
+		mudar_valor_acoes(carta.nicho, carta.informacao)
 
-	# Exibir ações atualizadas
+	exibir_carta(cartas_sorteadas[0])
+
 	print("---------------------------------------------------------------------------------------------")
-	print("Ações atualizadas:")
+	print("Ações após aplicação da carta:")
 	for acao in acoes_lista:
 		print("Nicho: %s, Preço: %f" % [acao.nicho, acao.preco])
-
-	# Teste de menu de ações
-	menu.atualizar_preco_em_tempo_real("Tecnologia", 3)
-	menu.comprar_acao()
-	print("Saldo final: %f" % saldo.puxar_saldo())
